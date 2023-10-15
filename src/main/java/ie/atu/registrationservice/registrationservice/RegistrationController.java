911d9e812884093278bdc461bcfb514ecf37d510
conorkeane <g00391846@atu.ie>
2023-10-15 15:22:41 +0100

    Create a method in RegistrationController to handle user registration requests. Inside this method, process the registration and return a confirmation message

--- src/main/java/ie/atu/registrationservice/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/registrationservice/RegistrationController.java
@@ -1,4 +1,14 @@
 package ie.atu.registrationservice.registrationservice;
 
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RegistrationController {
+    @PostMapping("/register")
+    public String registerUser(@RequestBody UserDetails userDetails) {
+        return "Confirm register";
+    }
 }
