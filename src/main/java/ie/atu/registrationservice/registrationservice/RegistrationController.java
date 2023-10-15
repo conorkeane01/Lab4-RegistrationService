05bd95a03cee8950a82180c16c952a4d9963c29e
conorkeane <g00391846@atu.ie>
2023-10-15 19:10:55 +0100

    Changed Postmap from register to confirm

--- src/main/java/ie/atu/registrationservice/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/registrationservice/RegistrationController.java
@@ -5,10 +5,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
 @RestController
 public class RegistrationController {
     @PostMapping("/register")
     public String registerUser(@RequestBody UserDetails userDetails) {
         return "Confirm register";
     }
+}
+
+ */
+
+
+
+@RestController
+public class RegistrationController {
+    @PostMapping("/confirm")
+    public String registerUser(@RequestBody UserDetails userDetails) {
+        String confirmationMessage = String.format("Received details for %s with email %s", userDetails.getName(), userDetails.getEmail());
+        return confirmationMessage;
+    }
+
 }
