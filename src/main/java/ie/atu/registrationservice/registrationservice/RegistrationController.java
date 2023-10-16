04f0e6707a2560a24c89d524872c598c8e8da4ea
conorkeane <g00391846@atu.ie>
2023-10-16 18:27:36 +0100

    Added NotificationServiceClient and added feign and get mapping

--- src/main/java/ie/atu/registrationservice/registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/registrationservice/registrationservice/RegistrationController.java
@@ -1,6 +1,7 @@
 package ie.atu.registrationservice.registrationservice;
 
 import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
+    private NotificationServiceClient notificationServiceClient;
+
+    @Autowired
+    public RegistrationController(NotificationServiceClient notificationServiceClient) {
+        this.notificationServiceClient = notificationServiceClient;
+    }
     @PostMapping("/confirm")
     public String registerUser(@RequestBody UserDetails userDetails) {
-        String confirmationMessage = String.format("Received details for %s ", userDetails.getName(), userDetails.getEmail());
+        String notification = notificationServiceClient.someDetails(userDetails);
+        String confirmationMessage = String.format("Received details for %s " + notification, userDetails.getName());
         return confirmationMessage;
     }
 
